e26d4bf66348f0064aae3060533271e18df32b42
Vaishnavi Chaudhari <vaishnavichaudhari0645@gmail.com>
2022-08-14 11:27:40 +0000

    Day 2

--- Patterns/fifth_pattern.java
+++ Patterns/fifth_pattern.java
@@ -1,5 +1,28 @@
+/*
+   1
+  12
+ 345
+4567
+ */
 package Patterns;
 
-public class fifth_pattern {
-    
+import java.util.Scanner;
+
+class fifth_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int val = 1;
+        for(int i = 1; i <= n; i++){
+            for(int space = 1; space <= n - i; space++){
+                System.out.print(" ");
+            }
+                for(int j = 0; j < i; j++){
+                        val = i + j;
+                        System.out.print(val);
+                    }
+                System.out.println();
+        }
+    sc.close();
+    }
 }
