6d15d5a50c4dfc2b8e6909e1b71eff1cebc711c5
Vaishnavi Chaudhari <vaishnavichaudhari0645@gmail.com>
2022-08-14 06:35:18 +0000

    Day 2

---
+++ Patterns/first_pattern.java
@@ -0,0 +1,25 @@
+/* 
+1
+12
+123
+1234
+*/
+
+package Patterns;
+
+import java.util.Scanner;
+
+class first_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    sc.close();
+    }
+    
+}
