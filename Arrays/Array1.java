419d60e8f629bfb943eb0560643d58d9c4adfc2f
Vaishnavi Chaudhari <vaishnavichaudhari0645@gmail.com>
2022-08-18 07:18:05 +0000

    Day 4

---
+++ Arrays/Array1.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Scanner;
+
+
+class Array1 {
+    public static int[] getData() {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        return arr;
+    }
+
+    public static void printArr(int arr[]) {
+        int size = arr.length;
+        for(int i = 0; i < size; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = getData();
+        printArr(arr);
+    }
+}
